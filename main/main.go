253c635a8ab6ac89b5acd3d0feda71a4a799bcb1
virullius <virullius@gmail.com>
2021-11-24 01:37:49 -0600

    MVP

---
+++ main/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+  "flag"
+  "mkpass"
+  "fmt"
+  "os"
+)
+
+var (
+  printVersion bool
+  xUpper, xLower, xNumber, xSymbol bool
+  length, count int
+)
+
+func init() {
+  flag.BoolVar(&printVersion, "version", false, "print version and quit")
+  flag.BoolVar(&xUpper, "xu", false, "exclude upper case characters")
+  flag.BoolVar(&xLower, "xl", false, "exclude lower case characters")
+  flag.BoolVar(&xNumber, "xn", false, "exclude numeric characters")
+  flag.BoolVar(&xSymbol, "xs", false, "exclude symbol characters")
+  flag.IntVar(&length, "l", 16, "length of each generated string")
+  flag.IntVar(&count, "n", 1, "number of strings to genereate")
+}
+
+func main() {
+  flag.Parse()
+  if printVersion {
+    fmt.Println(mkpass.VERSION)
+    os.Exit(0)
+  }
+  opt := mkpass.Options{
+    Upper: !xUpper,
+    Lower: !xLower,
+    Number: !xNumber,
+    Symbol: !xSymbol,
+    Length: length,
+  }
+  for i := 0; i < count; i++ {
+    s, err := mkpass.Generate(opt)
+    if err != nil {
+      fmt.Fprintf(os.Stderr, err.Error())
+      os.Exit(1)
+    }
+    fmt.Println(s)
+  }
+}
---
+++ mkpass.go
@@ -0,0 +1,96 @@
+package mkpass
+
+import (
+  "fmt"
+  "strings"
+  "crypto/rand"
+  "math/big"
+)
+
+const (
+  VERSION string = "0.1.0"
+  MAX_TRIES int = 10
+
+  UPPER string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  LOWER string = "abcdefghijklmnopqrstuvwxyz"
+  NUMBER string = "0123456789"
+  SYMBOL string = "`~!@#$%^&*()_+-=[]\\{}|;':\",./<>?"
+)
+
+var (
+  DefaultOptions Options = Options{
+    Upper: true,
+    Lower: true,
+    Number: true,
+    Symbol: true,
+    Length: 16,
+  }
+)
+
+type Options struct {
+  Upper bool
+  Lower bool
+  Number bool
+  Symbol bool
+  Length int
+}
+
+func Generate(o Options) (string, error) {
+  var (
+    charset = ""
+    output = ""
+  )
+  if o.Upper {
+    charset += UPPER
+  }
+  if o.Lower {
+    charset += LOWER
+  }
+  if o.Number {
+    charset += NUMBER
+  }
+  if o.Symbol {
+    charset += SYMBOL
+  }
+  if o.Length == 0 {
+    o.Length = DefaultOptions.Length
+  }
+
+  tries := 0
+
+  for {
+    tries++
+    // circuit breaker for uncaught errors that prevent generation of suitable strings
+    if tries > MAX_TRIES {
+      return "", fmt.Errorf("Failed to generate suitable random string in %d tries", MAX_TRIES)
+    }
+
+    output = ""
+    // pick random characters from set one at a time
+    l := big.NewInt(int64(len(charset)))
+    for i := 0; i < o.Length; i++ {
+      n, err := rand.Int(rand.Reader, l)
+      if err != nil {
+        return "", fmt.Errorf("Reading random integer: %s", err.Error())
+      }
+      output += string(charset[n.Int64()])
+    }
+
+    // check if each used class is included in output
+    if o.Upper && !strings.ContainsAny(output, UPPER) {
+      continue
+    }
+    if o.Lower && !strings.ContainsAny(output, LOWER) {
+      continue
+    }
+    if o.Number && !strings.ContainsAny(output, NUMBER) {
+      continue
+    }
+    if o.Symbol && !strings.ContainsAny(output, SYMBOL) {
+      continue
+    }
+    break
+  }
+
+  return output, nil
+}
