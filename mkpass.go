3c23111a6a54098d6f379fbbde842183267cd2c6
virullius <virullius@gmail.com>
2021-11-24 02:00:14 -0600

    fmt

--- mkpass.go
+++ mkpass.go
@@ -1,96 +1,99 @@
 package mkpass
 
 import (
-  "fmt"
-  "strings"
-  "crypto/rand"
-  "math/big"
+	"crypto/rand"
+	"fmt"
+	"math/big"
+	"strings"
 )
 
 const (
-  VERSION string = "0.1.0"
-  MAX_TRIES int = 10
+	VERSION   string = "0.1.0"
+	MAX_TRIES int    = 10
 
-  UPPER string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  LOWER string = "abcdefghijklmnopqrstuvwxyz"
-  NUMBER string = "0123456789"
-  SYMBOL string = "`~!@#$%^&*()_+-=[]\\{}|;':\",./<>?"
+	UPPER  string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	LOWER  string = "abcdefghijklmnopqrstuvwxyz"
+	NUMBER string = "0123456789"
+	SYMBOL string = "`~!@#$%^&*()_+-=[]\\{}|;':\",./<>?"
 )
 
 var (
-  DefaultOptions Options = Options{
-    Upper: true,
-    Lower: true,
-    Number: true,
-    Symbol: true,
-    Length: 16,
-  }
+	DefaultOptions Options = Options{
+		Upper:  true,
+		Lower:  true,
+		Number: true,
+		Symbol: true,
+		Length: 16,
+	}
 )
 
 type Options struct {
-  Upper bool
-  Lower bool
-  Number bool
-  Symbol bool
-  Length int
+	Upper  bool
+	Lower  bool
+	Number bool
+	Symbol bool
+	Length int
 }
 
 func Generate(o Options) (string, error) {
-  var (
-    charset = ""
-    output = ""
-  )
-  if o.Upper {
-    charset += UPPER
-  }
-  if o.Lower {
-    charset += LOWER
-  }
-  if o.Number {
-    charset += NUMBER
-  }
-  if o.Symbol {
-    charset += SYMBOL
-  }
-  if o.Length == 0 {
-    o.Length = DefaultOptions.Length
-  }
+	var (
+		charset = ""
+		output  = ""
+	)
+	if o.Upper {
+		charset += UPPER
+	}
+	if o.Lower {
+		charset += LOWER
+	}
+	if o.Number {
+		charset += NUMBER
+	}
+	if o.Symbol {
+		charset += SYMBOL
+	}
+	if o.Length == 0 {
+		o.Length = DefaultOptions.Length
+	}
+	if len(charset) == 0 {
+		return "", fmt.Errorf("No character classes choosen, characterset is empty")
+	}
 
-  tries := 0
+	tries := 0
 
-  for {
-    tries++
-    // circuit breaker for uncaught errors that prevent generation of suitable strings
-    if tries > MAX_TRIES {
-      return "", fmt.Errorf("Failed to generate suitable random string in %d tries", MAX_TRIES)
-    }
+	for {
+		tries++
+		// circuit breaker for uncaught errors that prevent generation of suitable strings
+		if tries > MAX_TRIES {
+			return "", fmt.Errorf("Failed to generate suitable random string in %d tries", MAX_TRIES)
+		}
 
-    output = ""
-    // pick random characters from set one at a time
-    l := big.NewInt(int64(len(charset)))
-    for i := 0; i < o.Length; i++ {
-      n, err := rand.Int(rand.Reader, l)
-      if err != nil {
-        return "", fmt.Errorf("Reading random integer: %s", err.Error())
-      }
-      output += string(charset[n.Int64()])
-    }
+		output = ""
+		// pick random characters from set one at a time
+		l := big.NewInt(int64(len(charset)))
+		for i := 0; i < o.Length; i++ {
+			n, err := rand.Int(rand.Reader, l)
+			if err != nil {
+				return "", fmt.Errorf("Reading random integer: %s", err.Error())
+			}
+			output += string(charset[n.Int64()])
+		}
 
-    // check if each used class is included in output
-    if o.Upper && !strings.ContainsAny(output, UPPER) {
-      continue
-    }
-    if o.Lower && !strings.ContainsAny(output, LOWER) {
-      continue
-    }
-    if o.Number && !strings.ContainsAny(output, NUMBER) {
-      continue
-    }
-    if o.Symbol && !strings.ContainsAny(output, SYMBOL) {
-      continue
-    }
-    break
-  }
+		// check if each used class is included in output
+		if o.Upper && !strings.ContainsAny(output, UPPER) {
+			continue
+		}
+		if o.Lower && !strings.ContainsAny(output, LOWER) {
+			continue
+		}
+		if o.Number && !strings.ContainsAny(output, NUMBER) {
+			continue
+		}
+		if o.Symbol && !strings.ContainsAny(output, SYMBOL) {
+			continue
+		}
+		break
+	}
 
-  return output, nil
+	return output, nil
 }
--- mkpass_test.go
+++ mkpass_test.go
@@ -1,95 +1,95 @@
 package mkpass
 
 import (
-  "testing"
-  "strings"
+	"strings"
+	"testing"
 )
 
 func TestGenerateDefault(t *testing.T) {
-  s, err := Generate(DefaultOptions)
-  if err != nil {
-    t.Fatal(err.Error())
-  }
-  if len(s) != DefaultOptions.Length {
-    t.Errorf("Expecting length %d, got %d", DefaultOptions.Length, len(s))
-  }
+	s, err := Generate(DefaultOptions)
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	if len(s) != DefaultOptions.Length {
+		t.Errorf("Expecting length %d, got %d", DefaultOptions.Length, len(s))
+	}
 }
 
 func TestGenerateEmptySet(t *testing.T) {
-  _, err := Generate(Options{})
-  if err == nil {
-    t.Errorf("Expecting empty charset error, received nil error")
-  }
+	_, err := Generate(Options{})
+	if err == nil {
+		t.Errorf("Expecting empty charset error, received nil error")
+	}
 }
 
 func TestGenerateUpperOnly(t *testing.T) {
-  s, err := Generate(Options{
-    Upper: true,
-  })
-  if err != nil {
-    t.Fatal(err.Error())
-  }
-  if strings.ContainsAny(s, LOWER) {
-    t.Errorf("Unexpected lower case character in %s", s)
-  }
-  if strings.ContainsAny(s, NUMBER) {
-    t.Errorf("Unexpected number character in %s", s)
-  }
-  if strings.ContainsAny(s, SYMBOL) {
-    t.Errorf("Unexpected symbol character in %s", s)
-  }
+	s, err := Generate(Options{
+		Upper: true,
+	})
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	if strings.ContainsAny(s, LOWER) {
+		t.Errorf("Unexpected lower case character in %s", s)
+	}
+	if strings.ContainsAny(s, NUMBER) {
+		t.Errorf("Unexpected number character in %s", s)
+	}
+	if strings.ContainsAny(s, SYMBOL) {
+		t.Errorf("Unexpected symbol character in %s", s)
+	}
 }
 
 func TestGenerateLowerOnly(t *testing.T) {
-  s, err := Generate(Options{
-    Lower: true,
-  })
-  if err != nil {
-    t.Fatal(err.Error())
-  }
-  if strings.ContainsAny(s, UPPER) {
-    t.Errorf("Unexpected upper case character in %s", s)
-  }
-  if strings.ContainsAny(s, NUMBER) {
-    t.Errorf("Unexpected number character in %s", s)
-  }
-  if strings.ContainsAny(s, SYMBOL) {
-    t.Errorf("Unexpected symbol character in %s", s)
-  }
+	s, err := Generate(Options{
+		Lower: true,
+	})
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	if strings.ContainsAny(s, UPPER) {
+		t.Errorf("Unexpected upper case character in %s", s)
+	}
+	if strings.ContainsAny(s, NUMBER) {
+		t.Errorf("Unexpected number character in %s", s)
+	}
+	if strings.ContainsAny(s, SYMBOL) {
+		t.Errorf("Unexpected symbol character in %s", s)
+	}
 }
 
 func TestGenerateNumberOnly(t *testing.T) {
-  s, err := Generate(Options{
-    Number: true,
-  })
-  if err != nil {
-    t.Fatal(err.Error())
-  }
-  if strings.ContainsAny(s, UPPER) {
-    t.Errorf("Unexpected upper case character in %s", s)
-  }
-  if strings.ContainsAny(s, LOWER) {
-    t.Errorf("Unexpected lower case character in %s", s)
-  }
-  if strings.ContainsAny(s, SYMBOL) {
-    t.Errorf("Unexpected symbol character in %s", s)
-  }
+	s, err := Generate(Options{
+		Number: true,
+	})
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	if strings.ContainsAny(s, UPPER) {
+		t.Errorf("Unexpected upper case character in %s", s)
+	}
+	if strings.ContainsAny(s, LOWER) {
+		t.Errorf("Unexpected lower case character in %s", s)
+	}
+	if strings.ContainsAny(s, SYMBOL) {
+		t.Errorf("Unexpected symbol character in %s", s)
+	}
 }
 
 func TestGenerateSymbolOnly(t *testing.T) {
-  s, err := Generate(Options{
-    Symbol: true,
-  })
-  if err != nil {
-    t.Fatal(err.Error())
-  }
-  if strings.ContainsAny(s, UPPER) {
-    t.Errorf("Unexpected upper case character in %s", s)
-  }
-  if strings.ContainsAny(s, LOWER) {
-    t.Errorf("Unexpected lower case character in %s", s)
-  }
-  if strings.ContainsAny(s, NUMBER) {
-    t.Errorf("Unexpected number character in %s", s)
-  }
+	s, err := Generate(Options{
+		Symbol: true,
+	})
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+	if strings.ContainsAny(s, UPPER) {
+		t.Errorf("Unexpected upper case character in %s", s)
+	}
+	if strings.ContainsAny(s, LOWER) {
+		t.Errorf("Unexpected lower case character in %s", s)
+	}
+	if strings.ContainsAny(s, NUMBER) {
+		t.Errorf("Unexpected number character in %s", s)
+	}
 }
